16f1538e759aea312ab12a0592760e698212a214
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-06-30 16:20:15 +0900

    create sample1-bot

---
+++ go.mod
@@ -0,0 +1,10 @@
+module github.com/xxarupakaxx/sample1-bot
+
+go 1.16
+
+require (
+	github.com/labstack/echo v3.3.10+incompatible // indirect
+	github.com/labstack/gommon v0.3.0 // indirect
+	github.com/line/line-bot-sdk-go v7.8.0+incompatible // indirect
+	golang.org/x/crypto v0.0.0-20210616213533-5ff15b29337e // indirect
+)
---
+++ main.go
@@ -0,0 +1,80 @@
+package main
+
+import (
+	"github.com/echo"
+	"github.com/echo/middleware"
+	"github.com/line/line-bot-sdk-go/linebot"
+	"log"
+	"os"
+	"strings"
+)
+
+func main() {
+	port:=os.Getenv("PORT")
+
+	if port == "" {
+		log.Fatal("$PORT must be set")
+	}
+	bot,err:=linebot.New("4328d1c958e11672e073e3195ef4eb3d","GoAYA2JDta1XkkHWwO2TaNy9u1l0eYB9mY8m8f2Xg0sGOqO4vJDQWYbEHCCsHBu/T6zHGpuID/fqvIykge0n9M36ER001L2rJrtZ9qj460KcaegFMCDWQnuvntu+YINz/nABTYqsYINfygkqrAsK/gdB04t89/1O/w1cDnyilFU=")
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	e:=echo.New()
+	e.Use(middleware.Logger())
+
+	e.POST("", func(c echo.Context) error {
+		events,err:=bot.ParseRequest(c.Request())
+		if err != nil {
+			if err == linebot.ErrInvalidSignature {
+				log.Print(err)
+			}
+			return err
+		}
+
+		var replyText string
+		replyText="cute"
+
+		var res string
+		res="ty"
+
+		var replySticker string
+		replySticker ="good moring"
+
+		responseSticker :=linebot.NewStickerMessage("11537", "52002757")
+
+		var replyImage string
+		replyImage="cat"
+
+		responseImage:=linebot.NewImageMessage("https://i.gyazo.com/2db8f85c496dd8f21a91eccc62ceee05.jpg", "https://i.gyazo.com/2db8f85c496dd8f21a91eccc62ceee05.jpg")
+
+		var replyLocation string
+		replyLocation ="Disney"
+
+		responseLocation:=linebot.NewLocationMessage("TokyoDisneyLand", "千葉県浦安市舞浜", 35.632896, 139.880394)
+
+		for _, event := range events {
+			if event.Type == linebot.EventTypeMessage {
+				switch message := event.Message.(type) {
+				case *linebot.TextMessage:
+					replyMessage:=message.Text
+
+					if strings.Contains(replyMessage, replyText) {
+						bot.ReplyMessage(event.ReplyToken,linebot.NewTextMessage(res)).Do()
+					}else if strings.Contains(replyMessage, replySticker) {
+						bot.ReplyMessage(event.ReplyToken,responseSticker).Do()
+					}else if strings.Contains(replyMessage, replyLocation) {
+						bot.ReplyMessage(event.ReplyToken,responseLocation).Do()
+					}
+
+					_,err=bot.ReplyMessage(event.ReplyToken,linebot.NewTextMessage(replyMessage)).Do()
+					if err != nil {
+						return err
+					}
+				}
+			}
+		}
+		return err
+	})
+}
