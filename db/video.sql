c09b8e444cfc723d68e0eb81ca9b4f86b69712b1
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-07-06 00:38:59 +0900

    videoのtableを作った

---
+++ db/video.sql
@@ -0,0 +1,9 @@
+DROP TABLE IF EXISTS video;
+CREATE TABLE IF NOT EXISTS video(
+    videoId VARCHAR(255),
+    published_at TIMESTAMP NOT NULL,
+    channelID VARCHAR(255),
+    title VARCHAR(255),
+    description VARCHAR(255),
+    channelTitle VARCHAR(255)
+);
