e352e59d28e7c782683a166d437d1758aa18398c
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-07-11 14:02:07 +0900

    a

--- main.go
+++ main.go
@@ -92,78 +92,7 @@ func main() {
 			return err
 		})
 	e.Start(":"+port)
-	/*router := gin.New()
-	router.Use(gin.Logger())
 
-	// LINE Messaging API ルーティング
-	router.POST("/callback", func(c *gin.Context) {
-		events, err := bot.ParseRequest(c.Request)
-		if err != nil {
-			if err == linebot.ErrInvalidSignature {
-				log.Print(err)
-			}
-			return
-		}
-
-		// "可愛い" 単語を含む場合、返信される
-		var replyText string
-		replyText = "可愛い"
-
-		// チャットの回答
-		var response string
-		response = "ありがとう！！"
-
-		// "おはよう" 単語を含む場合、返信される
-		var replySticker string
-		replySticker = "おはよう"
-
-		// スタンプで回答が来る
-		responseSticker := linebot.NewStickerMessage("11537", "52002757")
-
-		// "猫" 単語を含む場合、返信される
-		var replyImage string
-		replyImage = "猫"
-
-		// 猫の画像が表示される
-		responseImage := linebot.NewImageMessage("https://i.gyazo.com/2db8f85c496dd8f21a91eccc62ceee05.jpg", "https://i.gyazo.com/2db8f85c496dd8f21a91eccc62ceee05.jpg")
-
-		// "ディズニー" 単語を含む場合、返信される
-		var replyLocation string
-		replyLocation = "ディズニー"
-
-		// ディズニーが地図表示される
-		responseLocation := linebot.NewLocationMessage("東京ディズニーランド", "千葉県浦安市舞浜", 35.632896, 139.880394)
-
-		for _, event := range events {
-			// イベントがメッセージの受信だった場合
-			if event.Type == linebot.EventTypeMessage {
-				switch message := event.Message.(type) {
-				// メッセージがテキスト形式の場合
-				case *linebot.TextMessage:
-					replyMessage := message.Text
-					// テキストで返信されるケース
-					if strings.Contains(replyMessage, replyText) {
-						bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(response)).Do()
-						// スタンプで返信されるケース
-					} else if strings.Contains(replyMessage, replySticker) {
-						bot.ReplyMessage(event.ReplyToken, responseSticker).Do()
-						// 画像で返信されるケース
-					} else if strings.Contains(replyMessage, replyImage) {
-						bot.ReplyMessage(event.ReplyToken, responseImage).Do()
-						// 地図表示されるケース
-					} else if strings.Contains(replyMessage, replyLocation) {
-						bot.ReplyMessage(event.ReplyToken, responseLocation).Do()
-					}
-					// 上記意外は、おうむ返しで返信
-					_, err = bot.ReplyMessage(event.ReplyToken, linebot.NewTextMessage(replyMessage)).Do()
-					if err != nil {
-						log.Print(err)
-					}
-				}
-			}
-		}
-	})
-	router.Run(":" + port)*/
 }
 
 func lineHandler(c echo.Context) error {
@@ -227,26 +156,35 @@ func sendRestoInfo(bot *linebot.Client, e *linebot.Event) {
 	}
 }
 
+// response APIレスポンス
 type response struct {
-	Results results  `json:"results"`
+	Results results `json:"results"`
 }
 
+// results APIレスポンスの内容
 type results struct {
 	Shop []shop `json:"shop"`
 }
 
+// shop レストラン一覧
 type shop struct {
-	Name string `json:"name"`
+	Name    string `json:"name"`
 	Address string `json:"address"`
-	Photo photo `json:"photo"`
-	urls urls `json:"urls"`
+	Photo   photo  `json:"photo"`
+	URLS    urls   `json:"urls"`
 }
+
+// photo 写真URL一覧
 type photo struct {
 	Mobile mobile `json:"mobile"`
 }
+
+// mobile モバイル用の写真URL
 type mobile struct {
 	L string `json:"l"`
 }
+
+// urls URL一覧
 type urls struct {
 	PC string `json:"pc"`
 }
@@ -277,7 +215,7 @@ func getRestoInfo(lat string, lng string) []*linebot.CarouselColumn {
 			addr=string([]rune(addr)[:60])
 		}
 
-		cc:=linebot.NewCarouselColumn(shop.Photo.Mobile.L,shop.Name,addr,linebot.NewURIAction("ホットペッパーを開く",shop.urls.PC)).WithImageOptions("#FFFFFF")
+		cc:=linebot.NewCarouselColumn(shop.Photo.Mobile.L,shop.Name,addr,linebot.NewURIAction("ホットペッパーを開く",shop.URLS.PC)).WithImageOptions("#FFFFFF")
 		ccs=append(ccs,cc)
 	}
 
