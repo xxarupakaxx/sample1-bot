40ee72c9fcc9081983e3f8264ede613616d2488a
xxarupakaxx <arupaka3tdigks@gmail.com>
2021-07-17 15:30:06 +0900

    遊び実験

--- main.go
+++ main.go
@@ -104,7 +104,7 @@ func lineHandler(c echo.Context) error {
 
 			}
 			if message.Text=="tameda" {
-				//text:=user.DisplayName+"がtamedaと送信しました"
+				text:=user.DisplayName+"がtamedaと送信しました"
 				bot.ReplyMessage(event.ReplyToken,linebot.NewFlexMessage("メンテナンス終了",
 					&linebot.BubbleContainer{
 						Type: linebot.FlexContainerTypeBubble,
@@ -112,14 +112,26 @@ func lineHandler(c echo.Context) error {
 							Type:            linebot.FlexComponentTypeBox,
 							Layout:          linebot.FlexBoxLayoutTypeBaseline,
 							Contents:        []linebot.FlexComponent{
-								&linebot.ImageComponent{
-									Type:            linebot.FlexComponentTypeImage,
-									URL:             "https://pbs.twimg.com/profile_images/1364204318790836232/dgyZpvy6_400x400.jpg",
-									Gravity:         linebot.FlexComponentGravityTypeBottom,
-									Size:            linebot.FlexImageSizeTypeFull,
-									AspectRatio:     linebot.FlexImageAspectRatioType1to3,
-									AspectMode:      linebot.FlexImageAspectModeTypeFit,
-									BackgroundColor: "#f891df",
+								&linebot.TextComponent{
+									Type:       linebot.FlexComponentTypeText,
+									Text:       text,
+									Gravity:    linebot.FlexComponentGravityTypeTop,
+									Color:      "#e76565",
+									Style:      linebot.FlexTextStyleTypeNormal,
+									Decoration: linebot.FlexTextDecorationTypeUnderline,
+								},
+								&linebot.BoxComponent{
+									Type:            linebot.FlexComponentTypeBox,
+									Layout:          linebot.FlexBoxLayoutTypeHorizontal,
+									CornerRadius:    linebot.FlexComponentCornerRadiusTypeXl,
+									BackgroundColor: "#e2e765",
+								},
+								&linebot.ButtonComponent{
+									Type:    linebot.FlexComponentTypeButton,
+									Action:  linebot.NewMessageAction("tameda", "うーん"),
+									Style:   linebot.FlexButtonStyleTypeSecondary,
+									Color:   "#65bae7",
+									Gravity: linebot.FlexComponentGravityTypeBottom,
 								},
 							},
 							CornerRadius:  linebot.FlexComponentCornerRadiusTypeMd  ,
